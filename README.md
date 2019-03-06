d29eb9ce9ed4067449d6fc020618bf2c37b416eb
Mumtaz <mumtazahmadui@gmail.com>
2019-03-06 10:58:06 +0530

    final code

---
+++ README.md
@@ -0,0 +1,7 @@
+# set-up
+```
+git clone <this-repo>
+cd <this-repo>
+http-server -p 3000
+# visit localhost:3000 to see the todoListApp
+```
--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,3 @@
-angular.module('toDoApp', [])
-    .controller('mainCtrl', function ($scope) {
-        $scope.todo = [
-            { "name": "clean the house" },
-            { "name": "water the dog" },
-            { "name": "feed the lawn" },
-            { "name": "pay dem bills" },
-            { "name": "run" },
-            { "name": "swim" }
-        ]
-    });
+'use strict';
+
+angular.module('todoListApp', []);
